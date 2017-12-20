aa3de115995e825dee4b275ff479f9ae995b965a
Nick <drachenkony@gmail.com>
2017-12-21 01:48:37 +0200

    Added implementation of task_02

---
+++ task_02/main.py
@@ -0,0 +1,70 @@
+import os
+import argparse
+
+
+def count_words(func):
+
+    def wrapper(*args, **kwargs):
+        counters = dict()
+        source_data, search_data = func(*args, **kwargs)
+        for string in search_data:
+            counters[string] = source_data.count(string)
+        return counters
+
+    return wrapper
+
+
+@count_words
+def read_from_file(filepath):
+    source_data, search_data = list(), list()
+    list_to_append = source_data
+    if os.path.exists(filepath):
+        with open(filepath, 'r') as f:
+            line = f.readline()
+            while line:
+                line = line.strip()
+                if line != "":
+                    list_to_append.append(line)
+                else:
+                    list_to_append = search_data
+                line = f.readline()
+        return source_data, list(set(search_data))
+
+
+@count_words
+def get_from_shell():
+
+    rows_number = input("Введите количество строк исходных данных: ")
+    while not rows_number.isdigit() or rows_number == "0":
+        rows_number = input("Ошибка ввода! Повторите ввод: ")
+    source_data = [input() for _ in range(int(rows_number))]
+
+    rows_number = input("Введите количество строк для поиска: ")
+    while not rows_number.isdigit() or rows_number == "0":
+        rows_number = input("Ошибка ввода! Повторите ввод: ")
+    search_data = [input() for _ in range(int(rows_number))]
+
+    return source_data, list(set(search_data))
+
+
+def count_search_data(source_data, search_data):
+    counters = dict()
+    for string in search_data:
+        counters[string] = source_data.count(string)
+    return counters
+
+
+if __name__ == "__main__":
+    ap = argparse.ArgumentParser()
+    ap.add_argument("-f", "--file", help="path to the data file")
+    args = vars(ap.parse_args())
+    data = list()
+
+    if args.get("file", None) is not None:
+        counters = read_from_file(args['file'])
+        for k, v in counters.items():
+            print("{}: {}".format(k, v))
+    else:
+        counters = get_from_shell()
+        for k, v in counters.items():
+            print("{}: {}".format(k, v))
