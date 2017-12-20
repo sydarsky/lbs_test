a84a2d30f3c122576eecb03f8c5c74bba7226ba5
Nick <drachenkony@gmail.com>
2017-12-21 01:13:26 +0200

    Added input control

--- task_01/main.py
+++ task_01/main.py
@@ -19,12 +19,25 @@ def transform_output(func):
     return wrapper
 
 
+def pattern_match(data):
+    line_pattern = re.compile(r'^(?P<fname>[А-Я]?[а-я]*)\s' +
+                              r'(?P<sname>[А-Я]?[а-я]*)\s' +
+                              r'(?P<gender>[МЖ])\s' +
+                              r'(?P<age>\d{1,2})$')
+    for row in data:
+        if not line_pattern.search(row):
+            return False
+    return True
+
+
 @transform_output
 def read_from_file(filepath):
     data = None
     if os.path.exists(filepath):
         with open(filepath, 'r') as f:
             data = f.readlines()
+        if not pattern_match(data):
+            raise ValueError("Wrong line pattern -> {}".format(row))
         data = [i.replace("\n", "").split() for i in data]
     if data is not None:
         return sorted(data, key=lambda x: int(x[3]))
@@ -35,12 +48,15 @@ def get_from_shell():
     rows_number = input("Введите количество строк для ввода: ")
     while not rows_number.isdigit() or rows_number == "0":
         rows_number = input("Ошибка ввода! Повторите ввод: ")
-    data = [input().split() for _ in range(int(rows_number))]
+    print("Шаблон ввода: <Имя> <Фамилия> <Пол [М/Ж]> <Возраст>")
+    data = [input() for _ in range(int(rows_number))]
+    if not pattern_match(data):
+        raise ValueError("Wrong line pattern -> {}".format(row))
+    data = [row.split() for row in data]
     return sorted(data, key=lambda x: int(x[3]))
 
 
 if __name__ == "__main__":
-    line_pattern = re.compile(r'^(?P<fname>[А-Я]?[а-я]*)\s(?P<sname>[А-Я]?[а-я]*)\s(?P<sex>[МЖ])\s(?P<age>\d{1,2})$')
     ap = argparse.ArgumentParser()
     ap.add_argument("-f", "--file", help="path to the data file")
     args = vars(ap.parse_args())
